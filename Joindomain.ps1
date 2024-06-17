9134fdc2beea8a2567e962adf0250f9ba0b6c26b
lammertvandijk <166612858+lammertvandijk@users.noreply.github.com>
2024-06-17 14:49:30 +0200

    Update Joindomain.ps1

--- Joindomain.ps1
+++ Joindomain.ps1
@@ -4,11 +4,8 @@ param (
     [string]$parDomainPassword
 )
 
-# Convert plain text password to secure string
-$securePassword = ConvertTo-SecureString -String $parDomainPassword -AsPlainText -Force
-
 # Create the credential object
-$credential = New-Object System.Management.Automation.PSCredential($parDomainUsername, $securePassword)
+$credential = New-Object System.Management.Automation.PSCredential($parDomainUsername, $parDomainPassword)
 
 # Join the domain
 Add-Computer -DomainName $outParDomainFQDN -Credential $credential -Restart
