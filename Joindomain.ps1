92d755b6ae9adcc2dc038bf59cf22f059cecb028
lammertvandijk <166612858+lammertvandijk@users.noreply.github.com>
2024-07-01 15:48:46 +0200

    Update Joindomain.ps1

--- Joindomain.ps1
+++ Joindomain.ps1
@@ -7,4 +7,13 @@ param (
 $securePassword = ConvertTo-SecureString $domainPassword -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential ($domainUsername, $securePassword)
 
+Write-Output "Credentials created" | Out-File -FilePath C:\domainjoin.log -Append
+
+try {
+    Write-Output "Joining domain $domainName" | Out-File -FilePath C:\domainjoin.log -Append
+    Add-Computer -DomainName $domainName -Credential $credential -Restart
+} catch {
+    Write-Error "Error joining domain: $_" | Out-File -FilePath C:\domainjoin.log -Append
+}
+
 Add-Computer -DomainName $domainServicesName -Credential $credential -Restart
