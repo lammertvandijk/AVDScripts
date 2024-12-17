7a97958a0cd98a4d4d0095a1185e72ecc74f0e70
lammertvandijk <166612858+lammertvandijk@users.noreply.github.com>
2024-12-17 13:42:21 +0100

    Update validateKeyVaultName.ps1

--- validateKeyVaultName.ps1
+++ validateKeyVaultName.ps1
@@ -1,8 +1,4 @@
-#source: https://medium.com/@TimGroothuis/validating-azure-keyvault-names-with-powershell-3d672a40efa1
-#The account with which you generate the token needs to have enough permissions (KeyVault Contributor should suffice).
-#the code uses the current connected subscriptionId
-
-#paramter passtrough
+#parameter passthrough
 param (
     [Parameter(Mandatory=$True)][string]$KeyVaultName # The Vault name to check the availability
 )
@@ -12,87 +8,82 @@ function validateKeyVaultName {
         [Parameter(Mandatory=$True)][string]$KeyVaultName # The Vault name to check the availability
     )
 
-    # Validating KeyVault name length requirements
-if($KeyVaultName.Length -lt 3){
-    Write-Host "Azure KeyVault names should be at least 3 charcters long"
-    break
-} elseif($KeyVaultName.Length -gt 24){
-    Write-Host "Azure KeyVault names can't be longer than 24 charcters"
-    break
-}
+    # Validating KeyVault name requirements
+    if ($KeyVaultName.Length -lt 3) {
+        Write-Host "Azure KeyVault names should be at least 3 characters long"
+        return $false
+    } elseif ($KeyVaultName.Length -gt 24) {
+        Write-Host "Azure KeyVault names can't be longer than 24 characters"
+        return $false
+    }
 
-# Validating KeyVault hyphen requirements
-if($KeyVaultName -match "--"){
-    Write-Host "Azure KeyVault names can't contain consecutive hyphens"
-    break
-}
+    if ($KeyVaultName -match "--") {
+        Write-Host "Azure KeyVault names can't contain consecutive hyphens"
+        return $false
+    }
 
-# Validating Keyvault starting character requirements
-if(-not($KeyVaultName -match "^[A-Z]")){
-    Write-Host "Azure KeyVault names must start with a letter"
-    break
-}
+    if (-not ($KeyVaultName -match "^[A-Z]")) {
+        Write-Host "Azure KeyVault names must start with a letter"
+        return $false
+    }
 
-# Validating Keyvault ending character requirements
-if(-not($KeyVaultName -match "\w$")){
-    Write-Host "Azure KeyVault names must end with a letter or digit"
-    break
-}
+    if (-not ($KeyVaultName -match "\w$")) {
+        Write-Host "Azure KeyVault names must end with a letter or digit"
+        return $false
+    }
+
+    return $true
 }
 
 function validateKeyVaultAvailability {
     param (
         [Parameter(Mandatory=$True)][string]$KeyVaultName # The Vault name to check the availability
     )
+
     # Validate KeyVaultName
-    validateKeyVaultName -KeyVaultName $KeyVaultName
-    
+    $isNameValid = validateKeyVaultName -KeyVaultName $KeyVaultName
+    if (-not $isNameValid) {
+        return $false
+    }
 
     # Getting the token of your current session
     $token = (Get-AzAccessToken).Token
 
-
-    #authorization header build (hash table)
+    # Authorization header build (hash table)
     $headers = @{
-         Authorization = "Bearer $token"
+        Authorization = "Bearer $token"
     }
 
-    #request body build (hash table)
+    # Request body build (hash table)
     $body = @{
-        "name" = "$KeyVaultName";
+        "name" = "$KeyVaultName"
         "type" = "Microsoft.KeyVault/vaults"
     }
 
-    #Getting the SubscriptionId of the subscription currently in use
-    $subscriptionId = (Get-AzContext).Subscription.Id 
+    # Getting the SubscriptionId of the subscription currently in use
+    $subscriptionId = (Get-AzContext).Subscription.Id
 
-    # build the uri 
+    # Build the URI
     $uri = "https://management.azure.com/subscriptions/$subscriptionId/providers/Microsoft.KeyVault/checkNameAvailability?api-version=2022-07-01"
 
-    # Making the call to the Keyvault checkNameAvailability API and cathing the response in a variable
+    # Making the call to the KeyVault checkNameAvailability API
     $response = Invoke-WebRequest -Method Post -Uri $uri -Headers $headers -Body $body -ErrorAction Stop
 
-
-    #response
-    $output = if($response.Content -match "true"){
-        Write-Host "true"
+    # Response handling
+    if ($response.Content -match "true") {
+        return $true
     } else {
-         Write-Host "false"
-    } 
+        return $false
+    }
+}
 
-Write-Output $output
-# Output the result in JSON format
-$output | ConvertTo-Json -Depth 10
+# Validate KeyVault availability
+$isAvailable = validateKeyVaultAvailability -KeyVaultName $KeyVaultName
 
+# Prepare output
 $DeploymentScriptOutputs = @{
-    KeyVaultAvailability = $output
+    KeyVaultAvailability = $isAvailable
 }
 
-}
-
-validateKeyVaultAvailability -KeyVaultName $KeyVaultName
-
-
-
-
-
+# Output the result
+Write-Output ($DeploymentScriptOutputs | ConvertTo-Json -Depth 1)
